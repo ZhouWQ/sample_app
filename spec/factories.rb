21c79358062a8f7bcefa9a7b3b948fdc77a5152e
zhouwq <lion.zhou326@gmail.com>
2013-09-24 17:21:22 +0800

    Finish user edit, update, index, and destory actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,13 @@
 FactoryGirl.define do
-	factory:user do
-		name "zhouwq"
-		email "zhouwq@pset.com"
+	factory :user do
+		sequence (:name) { |n| "Person_#{n}" }
+		sequence (:email) { |n| "Person_#{n}@e.com"}
 		password "123456"
 		password_confirmation "123456"
+
+		factory :admin do
+			admin true
+		end
 	end
+
 end
